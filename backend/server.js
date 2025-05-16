716910f570b003c53a8112c0d87069f6f3218f68
Mohamed-Amine-Nafia <aminenafia05@gmail.com>
2025-05-16 23:09:33 +0200

    Add delete and mission done functionality to dashboard

--- backend/server.js
+++ backend/server.js
@@ -52,4 +52,5 @@ app.patch("/api/customers/:id", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3001, () => console.log("Server running on http://localhost:3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
